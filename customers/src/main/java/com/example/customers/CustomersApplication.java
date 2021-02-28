1b62dbc223079f49f12d7f3133717d93179a9a16
ch <mkmsacc@outlook.com>
2021-02-28 22:11:28 +0700

    Add websocket refreshscope

--- customers/src/main/java/com/example/customers/CustomersApplication.java
+++ customers/src/main/java/com/example/customers/CustomersApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
@@ -18,6 +19,7 @@ import reactor.core.publisher.Mono;
 import java.awt.*;
 import java.time.Duration;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
@@ -95,4 +97,14 @@ class CustomerRestController {
 class Customer {
     private Integer id;
     private String name;
+}
+
+@RestController
+class ReliabilityRestController {
+    private final Map<String, AtomicInteger> countOfErrors =
+            new ConcurrentHashMap<>();
+
+    @GetMapping("/error/{id}")
+    ResponseEntity<?>
+
 }
